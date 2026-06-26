317c851b4154c54ca4796c022dacc03e2b07d096
PETER <PETER@GMAIL.COM>
2026-06-26 11:33:55 -0400

    Fix: use @sentry/browser for client config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,9 +1,6 @@
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/browser";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  replaysSessionSampleRate: 0.01,
-  integrations: [Sentry.replayIntegration()],
 });
